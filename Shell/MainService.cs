38084b4d8689d7a43d670cdbbde6339fe1ef5015
SkyeBlack <296471989@qq.com>
2019-05-23 09:57:59 +0800

    Show error when PluginURL is not defined

--- Shell/MainService.cs
+++ Shell/MainService.cs
@@ -1411,6 +1411,12 @@ namespace Bhp.Shell
 
             if (!File.Exists(pluginName))
             {
+                if (string.IsNullOrEmpty(Settings.Default.PluginURL))
+                {
+                    Console.WriteLine("You must define `PluginURL` in your `config.json`");
+                    return true;
+                }
+
                 var address = string.Format(Settings.Default.PluginURL, pluginName, typeof(Plugin).Assembly.GetVersion());
                 fileName = Path.Combine(Path.GetTempPath(), $"{pluginName}.zip");
                 isTemp = true;
