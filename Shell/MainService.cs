7faccbc89eb8140ffe25a4d18ecdc098286da124
BhpAlpha <bhp-project@hotmail.com>
2019-08-23 10:13:29 +0800

    Fix invoke failed

--- Shell/MainService.cs
+++ Shell/MainService.cs
@@ -1701,7 +1701,7 @@ namespace Bhp.Shell
             if (tx.Inputs == null) tx.Inputs = new CoinReference[0];
             if (tx.Outputs == null) tx.Outputs = new TransactionOutput[0];
             if (tx.Witnesses == null) tx.Witnesses = new Witness[0];
-            ApplicationEngine engine = ApplicationEngine.Run(tx.Script, tx);
+            ApplicationEngine engine = ApplicationEngine.Run(tx.Script, tx, testMode: true);
 
             StringBuilder sb = new StringBuilder();
             sb.AppendLine($"VM State: {engine.State}");
